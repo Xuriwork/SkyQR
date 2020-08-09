fd59ff0ffe82580babb7ce5f51e40e25cde168ff
Xuri <xuriwork@gmail.com>
2020-08-09 10:11:36 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { SkynetClient } from 'skynet-js';
+import QRCode from 'qrcode.react';
+
+import UploadIcon from './assets/upload.svg';
+import SkyQRLogo from './assets/skyqr_logo.svg';
+import FolderIcon from './assets/folder.svg';
+
+const App = () => {
+	const [skylink, setSkylink] = useState(null);
+	const [file, setFile] = useState(null);
+	const [error, setError] = useState(null);
+	const [progress, setProgress] = useState(null);
+	const fileInputRef = useRef(null);
+
+	const handleOnFileChanged = (e) => setFile(e.target.files[0]);
+	const handleChooseFile = () => fileInputRef.current.click();
+	const onUploadProgress = (progress, { loaded, total }) =>
+		setProgress(Math.round(progress * 100));
+	const backToUpload = () => setSkylink(null);
+
+	const uploadFile = async () => {
+		try {
+			const client = new SkynetClient();
+			const { skylink: _skylink } = await client.upload(file, {
+				onUploadProgress,
+			});
+			setSkylink(_skylink);
+			console.log(_skylink);
+		} catch (_error) {
+			setError(_error);
+			console.log(error);
+		}
+	};
+
+	return (
+		<div className='app-component'>
+			<span className='header'>
+				<img src={SkyQRLogo} alt='logo' className='logo' />
+				<h1>SkyQR</h1>
+			</span>
+			<h2>
+				QR in the Sky<span style={{ color: '#57b560' }}>link</span>
+			</h2>
+			{skylink ? (
+				<div className='qrcode-container'>
+					<QRCode value={skylink} className='qrcode' fgColor='#57b560' />
+					<button onClick={backToUpload} className='back-to-upload-button'>
+						Back
+					</button>
+				</div>
+			) : (
+				<div className='upload-file-container'>
+					<div className='file-input-container'>
+						<input
+							type='file'
+							ref={fileInputRef}
+							accept='image/*'
+							onChange={handleOnFileChanged}
+							style={{ display: 'none' }}
+						/>
+						<button onClick={handleChooseFile} className='browse-button'>
+							<img src={FolderIcon} alt='' /> Browse
+						</button>
+						<button onClick={uploadFile} className='upload-button'>
+							<img src={UploadIcon} alt='' />
+							Upload
+						</button>
+					</div>
+					{file && <span className='filename'>Filename: {file.name}</span>}
+					{progress && <span className='progress'>Progress: {progress}</span>}
+				</div>
+			)}
+			{skylink && <span className='skylink'>Skylink: {skylink}</span>}
+			<a
+				className='skynet-link'
+				href='https://siasky.net'
+				target='_blank'
+				rel='noopener noreferrer'
+			>
+				POWERED BY SIA SKYNET{' '}
+				<span role='img' aria-label='Green heart'>
+					💚
+				</span>
+			</a>
+		</div>
+	);
+};
 
 export default App;
